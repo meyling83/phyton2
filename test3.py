b54ac0681375b7e2f32cd9538fe39ae8a2fb171d
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-02-16 16:46:45 +0100

    cambio 1

--- test3.py
+++ test3.py
@@ -1,1 +1,7 @@
 import requests
+currency="eur"
+basecurrency="aud"
+url="http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + currency + "&f=" + basecurrency + "&v=1&s=cbr"
+resp=requests.get[url]
+print(resp.text)
+print(resp.json())
