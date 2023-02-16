78db06c84768f537e8a16280772ed0b7f06883bc
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-02-16 16:21:01 +0000

    cambio 3

--- test3.py
+++ test3.py
@@ -1,7 +1,27 @@
-import requests
-currency="eur"
-basecurrency="aud"
-url="http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + currency + "&f=" + basecurrency + "&v=1&s=cbr"
-resp=requests.get(url)
-print(resp.text)
-print(resp.json())
+#from urllib.request import urlopen
+#page=urlopen("https://github.com/")
+#content=page.read()
+#print(content) 
+
+#acceder a api de cambio de moneda
+#import requests
+#currency="eur"
+#basecurrency="aud"
+#url="http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + currency + "&f=" + basecurrency + "&v=1&s=cbr"
+#resp=requests.get(url)
+#print(resp.text)
+#print(resp.json())
+
+#genera un identificador unico
+#import uuid
+#print ("El UUID uuid1() es: ",end="")
+#print(uuid.uuid1())
+
+#import turtle
+#myTurtle=turtle.Turtle()
+#myWin=turtle.Screen()
+#for i in range(4):
+    #myTurtle.forward(20)
+    #myTurtle.right(90)
+#myWin.exitonclick()
+
