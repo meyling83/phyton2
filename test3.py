83d0cfd1b5410f8444d18f803420ac48f377eca1
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-02-22 16:51:53 +0000

    cambios88

--- test3.py
+++ test3.py
@@ -152,7 +152,7 @@ print("muy bien las contraseñas coinciden") """
 """ while True:
     pw1=input("Introduce tu contraseña:")
     pw2=input("Introduce de nuevo tu contraseña:")
-    if pw1==pw2
+    if pw1==pw2:
         print("muy bien las contraseñas coinciden")
         break
 
@@ -160,9 +160,28 @@ i=1
 ch="*"
 while i<10:
     print(ch*i)
-    i=i+2 """
+    i=i+2 
+print("--------")
+i=9
+while i>0:
+    print(ch*i)
+    i=i-2
 
-""" s="    122,Python,es,64,un,777,lenguaje,222,de,55,66,programación  "
+#imprimir esterellas
+cadena="*"
+for i in range(5):
+    print(cadena)
+    cadena=cadena + "**"
+print("--------")
+for i in range(5):
+  
+    indice=len(cadena)-2
+    cadena=cadena[0:indice]
+    print(cadena)
+   
+
+
+s="    122,Python,es,64,un,777,lenguaje,222,de,55,66,programación  "
 s=s.strip()
 a=s.split(",")
 b=[] """
@@ -185,27 +204,115 @@ print(texto_final.upper())
 print(texto_final.lower()) """
 
 
-#imprimir esterellas
-""" cadena="*"
-for i in range(5):
-    print(cadena)
-    cadena=cadena + "**"
-print("--------")
-for i in range(5):
-  
-    indice=len(cadena)-2
-    cadena=cadena[0:indice]
-    print(cadena)
-   """
 
 #s="     Palabra    "
 #aux=s.strip()
 #s="       Palabra"
 #aux=s.lstrip()
 
-s="hola.mundo.como.estas"
+""" s="hola.mundo.como.estas"
 aux=s.split(".")
 aux1=" ".join(i for i in aux)
 print(aux)
-print(aux1)
+print(aux1) """
 
+""" #ejemplo list comprehension. Obtiene en una lista nueva los nombres que empiecen con j
+nombres=["jon","maria","juan","javier"]
+nueva_lista=[nombre for nombre in nombres if nombre[0]=="j"]
+print(nueva_lista)
+
+#list comprehension devolver una lista con numeros positivos
+numeros=[3,54,-12,4,-67,99,-120]
+lista_positiva=[numero for numero in numeros if numero>=0]
+lista_duplicar=[numero*2 for numero in numeros if numero>=0]
+print(lista_positiva)
+
+#list comprehension devolver una lista con numeros negativos
+lista_negativa=[numero for numero in numeros if numero<0]
+print(lista_negativa) """
+
+""" #con un bucle
+numeros=[3,54,-12,4,-67,99,-120]
+lista_positiva=[]
+lista_negativa=[]
+for numero in numeros:
+    if numero>=0:
+        lista_positiva.append(numero)
+    else:
+        lista_negativa.append(numero)
+
+print(lista_positiva) 
+print(lista_negativa) 
+"""
+
+""" 
+#reemplazar
+texto = "Pitón es un lenguaje de programación. Pitón está usado para la automación, análisis de datos e incluso la creación de páginas webs. Pitón fue creado por Bill Gates en 1960. Pitón es difícil de usar."
+texto=texto.replace("Pitón","Python")
+texto=texto.replace("difícil","facil")
+texto=texto.replace("1960","1990")
+texto=texto.replace("Bill Gates","Guido van Rossum")
+ 
+print(texto)
+
+texto = "    Lo más importante que nos ha mantenido en Python... bueno, hay 2 cosas importantes. Uno son las bibliotecas. La otra cosa que nos mantiene en Python, y esto es lo más importante, es facil de leer y entender. Cuando contratamos nuevos empleados. Solo digo, 'todo lo que escribas debe estar en python'. Sólo para que pueda leerlo. Y es increíble porque puedo ver desde el otro lado de la habitación, mirando su pantalla, si su código es bueno o malo. Porque un buen código de Python tiene una estructura muy obvia. Y eso hace que mi vida sea mucho más fácil   "
+#cantidad de veces que aparece python en el texto
+cantidad=texto.lower().count("python")
+
+#numero del caracter en el que encuentra python en el texto
+primera_ocurrencia=texto.lower().find("python")
+
+#segunda ocurrencia. me quedo con una subcadena a partir de la primera ocurrencia de python, para encontrar donde esta python en la subcadena, y sumo la longitud que le quite a la subcadena
+segunda_ocurrencia=texto[primera_ocurrencia+1:len(texto)-1].lower().find("python") + primera_ocurrencia+1
+
+print(cantidad)
+print(primera_ocurrencia)
+print(segunda_ocurrencia)
+
+#buscar si codigo esta en el texto
+if "código" in texto:
+    print("esta")
+else:
+    print("no esta")
+
+#reemplazar
+#texto=texto.replace("Python","PYTHON")
+print(texto)
+
+#quitar espacios al inicio y al final
+texto=texto.strip()
+print("X"+texto+"X")
+
+#convierte mayusculas a minusculas y viceversa
+texto=texto.swapcase()
+print(texto)
+ """
+
+#separa los nombres de los dominios
+""" emails = ["jon.smith@microsoft.com", "maria.fernandez@microsoft.com", "david@microsoft.com", "isabel@microsoft.es","alfonso@gmail.com"]
+nombres=""
+dominios=""
+for email in emails:
+    nombre,dominio=email.split("@")
+    if "." in nombre:
+        aux1=nombre.split(".")
+        nombre_completo=" ".join(i for i in aux1)
+    else:
+        nombre_completo=nombre
+    nombres=nombres+ "," + nombre_completo
+    if dominio not in dominios:
+       dominios=dominios + "," + dominio
+print(nombres)
+print(dominios)
+
+#devuelve un texto con nuevos correos
+nombres = ("maria", "jon", "david")
+texto=",".join(nombre+"@nazaret.eus" for nombre in nombres)
+print(texto) """
+
+#otra via de la segunda ocurrencia
+""" texto = "    Lo más importante que nos ha mantenido en Python... bueno, hay 2 cosas importantes. Uno son las bibliotecas. La otra cosa que nos mantiene en Python, y esto es lo más importante, es facil de leer y entender. Cuando contratamos nuevos empleados. Solo digo, 'todo lo que escribas debe estar en python'. Sólo para que pueda leerlo. Y es increíble porque puedo ver desde el otro lado de la habitación, mirando su pantalla, si su código es bueno o malo. Porque un buen código de Python tiene una estructura muy obvia. Y eso hace que mi vida sea mucho más fácil   "
+primera_ocurrencia=texto.lower().find("python")
+longitud=len("python")
+segunda_ocurrencia=texto.lower().find("python",primera_ocurrencia+longitud,len(texto)-1)
+print(segunda_ocurrencia) """
