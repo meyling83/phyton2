8166b891e97a1aa7f39b5913b9f7aa4c4fbca801
meyling83 <116660994+meyling83@users.noreply.github.com>
2023-03-29 17:54:05 +0200

    Update ejemplo_json.py

--- ejemplo_json.py
+++ ejemplo_json.py
@@ -1,9 +1,11 @@
 import json
 acciones={"AENA.MC":143.75,"BBVA.MC":6.34,"REP.MC":14.22,"SAN.MC":3.324}
+#convierte el diccionario en un string en formato json
 s=json.dumps(acciones)
 print(type(s))
 print(s)
 
+#convierte un string en formato json en un diccionario
 dicc=json.loads(s)
 print(type(dicc))
 print(dicc)
